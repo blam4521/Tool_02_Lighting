1736789a2b14a237f60219ad68e85cc00f8b46bd
blam4521 <blam452@gmail.com>
2015-10-05 13:56:50 -0400

    added get attr for ramps

--- CopyLight_Attr.py
+++ CopyLight_Attr.py
@@ -6,6 +6,7 @@ import itertools
 import time
 from datetime import date
 
+
 def RampForAllSelectedLights(allSelLights, path):
 	
 	'''
@@ -38,32 +39,41 @@ def RampForAllSelectedLights(allSelLights, path):
 		#file.write(LightSel)
 	
 		currShapes = cmds.listRelatives(j, c=True, s=True)
+		currColor = cmds.listConnections(currShapes, type='ramp')
+		#print currColor
+		mysTuple=[str(x) for x in currColor]
+		print "mysTuple: ", mysTuple
+		flatT = " ".join(str(x) for x in mysTuple)
+		print "flatT: ", flatT
+		Ramps.append(flatT)	
+	
 		
-		# Looping through all the shapes of the current light
-		for a in currShapes:
-			print 'currShapes:', a
-			currColor = cmds.listConnections(a, type='ramp')
-			
-			
-			
-			#point = cmds.getAttr(c+'.colorEntryList', multiIndices=1)
-			
-			#currColor = cmds.listConnections(currShapes, c=True, s=True, type="ramp")
-			#currColor = cmds.getAttr(currLightShape+".color" ,type=True)
-			
-			#print 'ramp is: ', point
-			#colorList.append(point)
-        #print colorList
-			
+		for i in Ramps:
+		    
+		    point = cmds.getAttr(i+'.colorEntryList', multiIndices=1)
+		    print "point: ", point
+		    for p in point: 
+			    p=str(p)
+			    
+			    colList = cmds.getAttr(i+'.colorEntryList['+p+'].color')
+			    posList = cmds.getAttr(i+'.colorEntryList['+p+'].position')
+			    cnum = list(colList)
+			    print "colList: ", cnum
+			    pval = posList
+			    print "posList: ", posList
+			    
+			    for c in cnum:
+					colPos =  """setAttr "%s.colorEntryList[%s].position" %f;"""%(i,p,pval)
+					colCol =  """setAttr "%s.colorEntryList[%s].color" -type double3 %f %f %f ;"""%(i,p,c[0],c[1],c[2]) 
+					colorList.append(colPos)
+					colorList.append(colCol)
+        print colorList			    
 '''			
-			if currColor != None:
-				point = cmds.getAttr(currColor+'.colorEntryList', multiIndices=1)
-				print point
-				colorList.append(point)
-			print colorList		
-'''
+		
+				
 
-'''
+
+
 			for p in currColor:
 				p = str(p)
 				colList = cmds.getAttr(currColor+'.colorEntryList['+p+'].color')
